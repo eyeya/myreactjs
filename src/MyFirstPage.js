d51b71bd11227ed8295b8e16bfbe97ecdcde024e
eyeya <86484894+eyeya@users.noreply.github.com>
2021-10-05 16:44:21 +0700

    Reactjs ครั้งสุดท้าย

--- src/MyFirstPage.js
+++ src/MyFirstPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Theme from "./components/Theme";
 export default function MyFirstPage() {
   const [clicks, setClicks] = useState(0);
   const [movies, setMovies] = useState([]);
@@ -44,54 +45,56 @@ export default function MyFirstPage() {
   };
 
   return (
-    <div>
-      <h1>Hello World !!! </h1>
-      <div className="container">
-        <div className="row">
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-          <div className="col">Column</div>
-        </div>
-      </div>
-      <div className="mb-4">
-        <button type="button" onClick={onButtonClick}>
-          Click me
-        </button>
-        <p>
-          Clicks: <span id="clicks">{clicks}</span>
-        </p>
-      </div>
-      <div className="mb-4">
-        <div>
-          <div>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={getMovies}
-            >
-              แสดง Movies
-            </button>
+    <Theme>
+      <div>
+        <h1>Hello World !!! </h1>
+        <div className="container">
+          <div className="row">
+            <div className="col">Column</div>
+            <div className="col">Column</div>
+            <div className="col">Column</div>
           </div>
-          <div className="row" id="movie-row">
-            {movies.map((item) => (
-              <div className="col-lg-4" key={item.id}>
-                <div className="card">
-                  <img
-                    src={item.image}
-                    className="card-img-top"
-                    height="250"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{item.title}</h5>
-                    <button className="btn btn-primary">Go somewhere</button>
+        </div>
+        <div className="mb-4">
+          <button type="button" onClick={onButtonClick}>
+            Click me
+          </button>
+          <p>
+            Clicks: <span id="clicks">{clicks}</span>
+          </p>
+        </div>
+        <div className="mb-4">
+          <div>
+            <div>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={getMovies}
+              >
+                แสดง Movies
+              </button>
+            </div>
+            <div className="row" id="movie-row">
+              {movies.map((item) => (
+                <div className="col-lg-4" key={item.id}>
+                  <div className="card">
+                    <img
+                      src={item.image}
+                      className="card-img-top"
+                      height="250"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.title}</h5>
+                      <button className="btn btn-primary">Go somewhere</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Theme>
   );
 }
